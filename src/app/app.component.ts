ddea054de740871a5380d3f3a5a2984f2d5071eb
Koshiz <koshilasandaru2@gmail.com>
2024-01-09 23:51:54 +0530

    Adding Song Adding Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-music-app';
+  musicGenre = [{name: 'testgenre1', description: 'This is my test1', type: 'classic'},
+                {name: 'testgenre2', description: 'This is my test2', type: 'rock'}];
+
+ addSong(addedSong: {name: string, description: string}){
+  this.musicGenre.push({
+    type: "rock",
+    name: addedSong.name,
+    description: addedSong.description
+  });
+
+ }
+
+              
 }
